d273ba397349f8ba190c982eb089069cbad1e5df
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-01-21 21:06:12 +0500

    woking on Onboardings

--- lib/utils/routes/routes_name.dart
+++ lib/utils/routes/routes_name.dart
@@ -3,4 +3,10 @@ class RoutesName {
   static const String login = 'Login_Screen';
   static const String home = 'Home_Screen';
   static const String signUp = 'Sign_up';
+
+  static const String onboarding1 = 'onboarding1';
+    static const String onboarding2 = 'onboarding2';
+
+  static const String onboarding3 = 'onboarding3';
+
 }
