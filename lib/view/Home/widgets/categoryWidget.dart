766c7d922231825438aab703e75732b99c538858
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-01-22 22:34:20 +0500

    working on Home

---
+++ lib/view/Home/widgets/categoryWidget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../../../res/components/colors.dart';
+
+class CategoryCart extends StatefulWidget {
+  final String label;
+
+  CategoryCart(this.label);
+
+  @override
+  _CategoryCartState createState() => _CategoryCartState();
+}
+
+class _CategoryCartState extends State<CategoryCart> {
+  Color _backgroundColor = AppColor.fieldBgColor;
+  Color _textColor = Colors.black; // Initial text color
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          // Toggle background color
+          _backgroundColor = (_backgroundColor == AppColor.fieldBgColor)
+              ? AppColor.secondaryColor
+              : AppColor.fieldBgColor;
+
+          // Toggle text color
+          _textColor =
+              (_textColor == Colors.black) ? Colors.white : Colors.black;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          height: 46,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          color: _backgroundColor,
+          child: Center(
+            child: Text(
+              widget.label,
+              style: TextStyle(fontSize: 16, color: _textColor),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/Login_view.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../res/components/rounded_button.dart';
+import '../utils/routes/routes_name.dart';
+import '../utils/routes/utils.dart';
+import '../view_model/auth_view_model.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final ValueNotifier<bool> _obscurePassword = ValueNotifier<bool>(true);
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final FocusNode _nameFocusNode = FocusNode();
+  final FocusNode _emailFocusNode = FocusNode();
+
+  final FocusNode _passwordFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _nameFocusNode.dispose();
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    _obscurePassword.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height * 1;
+    // final authViewModel = Provider.of<AuthViewModel>(context);
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('Login View'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                focusNode: _nameFocusNode,
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  hintText: 'name',
+                  label: Text('name'),
+                  prefixIcon: Icon(Icons.email),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+                onFieldSubmitted: (value) {
+                  Utils.focusNode(context, _nameFocusNode, _emailFocusNode);
+                },
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                focusNode: _emailFocusNode,
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  hintText: 'email',
+                  label: Text('email'),
+                  prefixIcon: Icon(Icons.email),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+                onFieldSubmitted: (value) {
+                  Utils.focusNode(context, _emailFocusNode, _passwordFocusNode);
+                },
+              ),
+              const SizedBox(height: 20.0),
+              ValueListenableBuilder(
+                  valueListenable: _obscurePassword,
+                  builder: (context, value, child) {
+                    return TextFormField(
+                      focusNode: _passwordFocusNode,
+                      obscureText: _obscurePassword.value,
+                      obscuringCharacter: '*',
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        hintText: 'password',
+                        label: const Text('password'),
+                        prefixIcon: const Icon(Icons.lock),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                        suffixIcon: InkWell(
+                          onTap: () {
+                            _obscurePassword.value = !_obscurePassword.value;
+                          },
+                          child: Icon(
+                            _obscurePassword.value
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+              SizedBox(height: height * .1),
+              RoundedButton(
+                title: 'Login',
+                loading: false,
+                onpress: () {},
+              ),
+              SizedBox(height: height * 0.02),
+              InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, RoutesName.signUp);
+                },
+                child: const Text("Don't have Account SignUP"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
