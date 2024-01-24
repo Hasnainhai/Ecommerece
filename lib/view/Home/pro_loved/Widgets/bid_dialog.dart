394a2205a0c93b1e990ef1b5ae49bdcfaae7b3b3
Basit ali <basitalyshah51214@gmail.com>
2024-01-23 23:27:19 -0800

    adding check out screen

--- lib/view/Home/pro_loved/Widgets/bid_dialog.dart
+++ lib/view/Home/pro_loved/Widgets/bid_dialog.dart
@@ -1,11 +1,19 @@
 import 'package:ecommerece/res/components/colors.dart';
+import 'package:ecommerece/res/components/custom_text_field.dart';
+import 'package:ecommerece/res/components/rounded_button.dart';
 import 'package:ecommerece/res/components/verticalSpacing.dart';
 import 'package:ecommerece/view/Home/pro_loved/Widgets/biding_title.dart';
 import 'package:flutter/material.dart';
 
-class BidDialog extends StatelessWidget {
+class BidDialog extends StatefulWidget {
   const BidDialog({super.key});
 
+  @override
+  State<BidDialog> createState() => _BidDialogState();
+}
+
+class _BidDialogState extends State<BidDialog> {
+  TextEditingController amountController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -15,14 +23,14 @@ class BidDialog extends StatelessWidget {
               color: AppColor.whiteColor,
               borderRadius: BorderRadius.circular(16),
             ),
-            child: const Padding(
-              padding: EdgeInsets.only(left: 12, right: 12),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 12, right: 12),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  VerticalSpeacing(30),
-                  Center(
+                  const VerticalSpeacing(30),
+                  const Center(
                     child: Text(
                       "Bidding",
                       style: TextStyle(
@@ -33,9 +41,9 @@ class BidDialog extends StatelessWidget {
                       ),
                     ),
                   ),
-                  VerticalSpeacing(34),
-                  Text(
-                    "Bidding",
+                  const VerticalSpeacing(34),
+                  const Text(
+                    "Product detail",
                     style: TextStyle(
                       fontFamily: 'CenturyGothic',
                       fontSize: 16,
@@ -43,10 +51,62 @@ class BidDialog extends StatelessWidget {
                       color: AppColor.fontColor,
                     ),
                   ),
-                  VerticalSpeacing(
+                  const VerticalSpeacing(
                     10,
                   ),
-                  BidingTile(),
+                  const BidingTile(),
+                  const VerticalSpeacing(
+                    24,
+                  ),
+                  const Text(
+                    "Delivery address",
+                    style: TextStyle(
+                      fontFamily: 'CenturyGothic',
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: AppColor.fontColor,
+                    ),
+                  ),
+                  const VerticalSpeacing(
+                    24,
+                  ),
+                  TextFieldCustom(
+                    controller: amountController,
+                    maxLines: 1,
+                    text: "Amount",
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please enter your name";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  const VerticalSpeacing(
+                    8,
+                  ),
+                  TextFieldCustom(
+                    controller: amountController,
+                    maxLines: 1,
+                    text: "Amount",
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please enter your name";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  const VerticalSpeacing(
+                    40,
+                  ),
+                  RoundedButton(
+                      title: "Place Order",
+                      onpress: () {},
+                      color: AppColor.primaryColor),
+                  const VerticalSpeacing(
+                    46,
+                  ),
                 ],
               ),
             )));
