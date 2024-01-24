bb175a1f108acdfbe0e84c8d5ba6183cbf261e6b
Basit ali <basitalyshah51214@gmail.com>
2024-01-24 01:34:53 -0800

    changes to fix the issues in the payment options

--- lib/view/Home/CheckOut/checkout_view.dart
+++ lib/view/Home/CheckOut/checkout_view.dart
@@ -13,6 +13,17 @@ class CheckOutView extends StatefulWidget {
 }
 
 class _CheckOutViewState extends State<CheckOutView> {
+  bool firstButton = true;
+  bool secondButton = false;
+  bool thirdButton = false;
+  bool isChecked = false;
+  String? address;
+  String? postalCode;
+  String? city;
+  String? state;
+  String? name;
+  String? phone;
+  String paymentType = 'Stripe';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,11 +53,10 @@ class _CheckOutViewState extends State<CheckOutView> {
         centerTitle: true,
       ),
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -86,10 +96,184 @@ class _CheckOutViewState extends State<CheckOutView> {
               ),
             ),
             const VerticalSpeacing(14),
-            FastShippingContainer()
-          ],
+            const FastShippingContainer(),
+            const VerticalSpeacing(14),
+            const Text(
+              'Payment option',
+              style: TextStyle(
+                fontFamily: 'CenturyGothic',
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: AppColor.blackColor,
+              ),
+            ),
+            SizedBox(
+                height: 66,
+                width: MediaQuery.of(context).size.width,
+                child: ListView(scrollDirection: Axis.horizontal, children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            firstButton = !firstButton;
+                            secondButton = false;
+                            thirdButton = false;
+                            paymentType = 'Stripe';
+                          });
+                        },
+                        child: Center(
+                          child: Container(
+                            height: 55,
+                            width: 113,
+                            decoration: BoxDecoration(
+                                color: firstButton
+                                    ? AppColor.primaryColor
+                                    : Colors.transparent,
+                                border: Border.all(
+                                    width: 1,
+                                    color: firstButton
+                                        ? AppColor.primaryColor
+                                        : AppColor.primaryColor)),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 28.0,
+                                  width: 28.0,
+                                  decoration: const BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage(
+                                              'images/masterCard.png'),
+                                          fit: BoxFit.contain)),
+                                ),
+                                const VerticalSpeacing(5),
+                                Text(
+                                  "Master Card",
+                                  style: TextStyle(
+                                      fontFamily: 'CenturyGothic',
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w300,
+                                      color: firstButton
+                                          ? AppColor.whiteColor
+                                          : AppColor.fontColor),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            firstButton = false;
+                            secondButton = false;
+                            thirdButton = !thirdButton;
+                            paymentType = 'cash on Delivery';
+                          });
+                        },
+                        child: Center(
+                          child: Container(
+                            height: 55,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              color: thirdButton
+                                  ? AppColor.primaryColor
+                                  : Colors.transparent,
+                              border: Border.all(
+                                  width: 1,
+                                  color: thirdButton
+                                      ? AppColor.primaryColor
+                                      : AppColor.primaryColor),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 28.0,
+                                  width: 28.0,
+                                  decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage('images/paypal.png'),
+                                        fit: BoxFit.contain),
+                                  ),
+                                ),
+                                const VerticalSpeacing(5),
+                                Text(
+                                  "PayPal",
+                                  style: TextStyle(
+                                    fontFamily: 'CenturyGothic',
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w300,
+                                    color: thirdButton
+                                        ? AppColor.whiteColor
+                                        : AppColor.fontColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            firstButton = false;
+                            secondButton = false;
+                            thirdButton = !thirdButton;
+                            paymentType = 'cash on Delivery';
+                          });
+                        },
+                        child: Center(
+                          child: Container(
+                            height: 55,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              color: thirdButton
+                                  ? AppColor.primaryColor
+                                  : Colors.transparent,
+                              border: Border.all(
+                                  width: 1,
+                                  color: thirdButton
+                                      ? AppColor.primaryColor
+                                      : AppColor.primaryColor),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 28.0,
+                                  width: 28.0,
+                                  decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage('images/cash.png'),
+                                        fit: BoxFit.contain),
+                                  ),
+                                ),
+                                const VerticalSpeacing(5),
+                                Text(
+                                  "Cash On Delivery",
+                                  style: TextStyle(
+                                    fontFamily: 'CenturyGothic',
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w300,
+                                    color: thirdButton
+                                        ? AppColor.whiteColor
+                                        : AppColor.fontColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ]))
+          ]),
         ),
-      )),
+      ),
     );
   }
 }
