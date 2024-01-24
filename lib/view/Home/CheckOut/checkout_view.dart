9059161b9843e19be5640e9a7645d7c13ab9bee0
Basit ali <basitalyshah51214@gmail.com>
2024-01-24 01:59:45 -0800

    chnages to add toggle

--- lib/view/Home/CheckOut/checkout_view.dart
+++ lib/view/Home/CheckOut/checkout_view.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerece/res/components/colors.dart';
+import 'package:ecommerece/res/components/custom_text_field.dart';
+import 'package:ecommerece/res/components/rounded_button.dart';
 import 'package:ecommerece/res/components/verticalSpacing.dart';
 import 'package:ecommerece/view/Home/CheckOut/Widgets/address_select_widget.dart';
 import 'package:ecommerece/view/Home/CheckOut/Widgets/fast_shipping_container.dart';
@@ -24,6 +26,15 @@ class _CheckOutViewState extends State<CheckOutView> {
   String? name;
   String? phone;
   String paymentType = 'Stripe';
+  bool switchValue = false;
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController cardNumberController = TextEditingController();
+
+  TextEditingController expDateController = TextEditingController();
+
+  TextEditingController csvController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,59 +66,60 @@ class _CheckOutViewState extends State<CheckOutView> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Select delivery address',
-                  style: TextStyle(
-                    fontFamily: 'CenturyGothic',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                    color: AppColor.blackColor,
-                  ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: const Text(
-                    'Add New',
+          child: SingleChildScrollView(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Select delivery address',
                     style: TextStyle(
                       fontFamily: 'CenturyGothic',
-                      fontSize: 14,
-                      fontWeight: FontWeight.w300,
-                      color: AppColor.primaryColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: AppColor.blackColor,
                     ),
                   ),
+                  InkWell(
+                    onTap: () {},
+                    child: const Text(
+                      'Add New',
+                      style: TextStyle(
+                        fontFamily: 'CenturyGothic',
+                        fontSize: 14,
+                        fontWeight: FontWeight.w300,
+                        color: AppColor.primaryColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const VerticalSpeacing(16),
+              const AddressSelectWidget(),
+              const VerticalSpeacing(20),
+              const Text(
+                'Shipping option',
+                style: TextStyle(
+                  fontFamily: 'CenturyGothic',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: AppColor.blackColor,
                 ),
-              ],
-            ),
-            const VerticalSpeacing(16),
-            const AddressSelectWidget(),
-            const VerticalSpeacing(20),
-            const Text(
-              'Shipping option',
-              style: TextStyle(
-                fontFamily: 'CenturyGothic',
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                color: AppColor.blackColor,
               ),
-            ),
-            const VerticalSpeacing(14),
-            const FastShippingContainer(),
-            const VerticalSpeacing(14),
-            const Text(
-              'Payment option',
-              style: TextStyle(
-                fontFamily: 'CenturyGothic',
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                color: AppColor.blackColor,
+              const VerticalSpeacing(14),
+              const FastShippingContainer(),
+              const VerticalSpeacing(14),
+              const Text(
+                'Payment option',
+                style: TextStyle(
+                  fontFamily: 'CenturyGothic',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: AppColor.blackColor,
+                ),
               ),
-            ),
-            SizedBox(
+              SizedBox(
                 height: 66,
                 width: MediaQuery.of(context).size.width,
                 child: ListView(scrollDirection: Axis.horizontal, children: [
@@ -272,8 +284,123 @@ class _CheckOutViewState extends State<CheckOutView> {
                       ),
                     ],
                   ),
-                ]))
-          ]),
+                ]),
+              ),
+              VerticalSpeacing(22),
+              TextFieldCustom(
+                controller: nameController,
+                maxLines: 1,
+                text: "Card Name",
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please enter your name";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              TextFieldCustom(
+                controller: cardNumberController,
+                maxLines: 1,
+                text: "Card Number",
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please enter your name";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFieldCustom(
+                      controller: expDateController,
+                      maxLines: 1,
+                      text: "Expire Date",
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter your name";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: TextFieldCustom(
+                      controller: csvController,
+                      maxLines: 1,
+                      text: "CSV",
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter your name";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const VerticalSpeacing(22),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Remeber My Card Details',
+                    style: TextStyle(
+                      fontFamily: 'CenturyGothic',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: AppColor.blackColor,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        switchValue = !switchValue;
+                      });
+                    },
+                    child: Container(
+                      width: 50.0,
+                      height: 26.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(0.0),
+                        color: switchValue
+                            ? AppColor.primaryColor
+                            : const Color(0xffECECEC),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: switchValue
+                            ? MainAxisAlignment.end
+                            : MainAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.only(left: 4, right: 4),
+                            width: 18.0,
+                            height: 18.0,
+                            decoration: const BoxDecoration(
+                              // shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const VerticalSpeacing(34),
+              RoundedButton(
+                  title: "Make payment pkr 2345",
+                  onpress: () {},
+                  color: AppColor.primaryColor),
+            ]),
+          ),
         ),
       ),
     );
