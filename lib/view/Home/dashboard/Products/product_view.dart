4ebb3671966d71790536af374177733ae21e966d
Basit ali <basitalyshah51214@gmail.com>
2024-01-26 11:24:54 -0800

    changes to add product screen

--- lib/view/Home/dashboard/Products/product_view.dart
+++ lib/view/Home/dashboard/Products/product_view.dart
@@ -1,5 +1,8 @@
 import 'package:ecommerece/res/components/colors.dart';
+import 'package:ecommerece/res/components/verticalSpacing.dart';
+import 'package:ecommerece/utils/routes/routes_name.dart';
 import 'package:ecommerece/view/Home/dashboard/dashboardScreen.dart';
+import 'package:ecommerece/view/Home/pro_loved/Widgets/pro_loved_card.dart';
 import 'package:ecommerece/view/filters/filters.dart';
 import 'package:flutter/material.dart';
 
@@ -45,38 +48,132 @@ class _ProductState extends State<Product> {
         padding: const EdgeInsets.all(
           20,
         ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 60,
-              width: (MediaQuery.of(context).size.width) - 40,
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Search Here",
-                  helperStyle: const TextStyle(color: AppColor.fieldBgColor),
-                  filled: true,
-                  border: InputBorder.none,
-                  prefixIcon: const Icon(
-                    Icons.search,
-                  ),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const FilterPopUp(),
-                        ),
-                      );
-                    },
-                    icon: const Icon(
-                      Icons.tune_sharp,
-                      color: AppColor.fontColor,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 60,
+                width: (MediaQuery.of(context).size.width) - 40,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Search Here",
+                    helperStyle: const TextStyle(color: AppColor.fieldBgColor),
+                    filled: true,
+                    border: InputBorder.none,
+                    prefixIcon: const Icon(
+                      Icons.search,
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const FilterPopUp(),
+                          ),
+                        );
+                      },
+                      icon: const Icon(
+                        Icons.tune_sharp,
+                        color: AppColor.fontColor,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+              const VerticalSpeacing(16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Populars',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: 'CenturyGothic',
+                      color: AppColor.fontColor,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, RoutesName.popularsScreen);
+                    },
+                    child: const Text(
+                      'see more',
+                      style: TextStyle(
+                          fontSize: 14.0,
+                          fontFamily: 'CenturyGothic',
+                          color: AppColor.fontColor,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ],
+              ),
+              const VerticalSpeacing(14),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                ),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return ProLovedCard(
+                    fun: () {
+                      Navigator.pushNamed(context, RoutesName.productdetail);
+                    },
+                  );
+                },
+              ),
+              const VerticalSpeacing(16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Our new items',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: 'CenturyGothic',
+                      color: AppColor.fontColor,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, RoutesName.newItemsScreen);
+                    },
+                    child: const Text(
+                      'see more',
+                      style: TextStyle(
+                          fontSize: 14.0,
+                          fontFamily: 'CenturyGothic',
+                          color: AppColor.fontColor,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ],
+              ),
+              const VerticalSpeacing(14),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                ),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return ProLovedCard(
+                    fun: () {
+                      Navigator.pushNamed(context, RoutesName.productdetail);
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       )),
     );
