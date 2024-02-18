2f9597a9ef49ec373830c3bf24a041fb32979c30
Basit ali <basitalyshah51214@gmail.com>
2024-02-18 07:58:20 -0800

    add default section

---
+++ lib/view/Home/widgets/default_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DefaultSection extends StatelessWidget {
+  const DefaultSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
