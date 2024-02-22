2727e81a0899fca55c9c860abe6ea0bbfc9d439d
Basit ali <basitalyshah51214@gmail.com>
2024-02-22 00:24:24 -0800

    changes to fix the issues of the app

--- lib/view/Home/widgets/storeWidget.dart
+++ lib/view/Home/widgets/storeWidget.dart
@@ -11,7 +11,10 @@ class StoreWidget extends StatelessWidget {
     required this.img,
     required this.rating,
     required this.address,
+    required this.ontap,
   });
+  final Function ontap;
+
   final String title;
   final String img;
   final String rating;
@@ -19,42 +22,47 @@ class StoreWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        isThreeLine: false,
-        leading: const CircleAvatar(
-          radius: 35.0,
-          backgroundColor: AppColor.primaryColor,
-          backgroundImage: NetworkImage(
-              'https://www.shutterstock.com/image-vector/red-store-vector-sign-promotion-260nw-1918121837.jpg'),
-        ),
-        title: Text(
-          title,
-          style: const TextStyle(
-              fontSize: 14.0,
-              color: AppColor.fontColor,
-              fontWeight: FontWeight.w600,
-              fontFamily: 'CenturyGothic'),
-        ),
-        subtitle: Text.rich(
-          TextSpan(
-              text: address,
-              style: const TextStyle(
-                  fontSize: 14.0,
-                  color: AppColor.fontColor,
-                  fontFamily: 'CenturyGothic'),
-              children: [
-                TextSpan(
-                  text: '\n⭐️ $rating',
-                  style: const TextStyle(
-                      fontSize: 14.0,
-                      color: AppColor.fontColor,
-                      fontFamily: 'CenturyGothic'),
-                )
-              ]),
-        ),
-        trailing: const Icon(
-          Icons.arrow_forward_ios_outlined,
-          color: AppColor.blackColor,
-        ));
+    return InkWell(
+      onTap: () {
+        ontap();
+      },
+      child: ListTile(
+          isThreeLine: false,
+          leading: const CircleAvatar(
+            radius: 35.0,
+            backgroundColor: AppColor.primaryColor,
+            backgroundImage: NetworkImage(
+                'https://www.shutterstock.com/image-vector/red-store-vector-sign-promotion-260nw-1918121837.jpg'),
+          ),
+          title: Text(
+            title,
+            style: const TextStyle(
+                fontSize: 14.0,
+                color: AppColor.fontColor,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'CenturyGothic'),
+          ),
+          subtitle: Text.rich(
+            TextSpan(
+                text: address,
+                style: const TextStyle(
+                    fontSize: 14.0,
+                    color: AppColor.fontColor,
+                    fontFamily: 'CenturyGothic'),
+                children: [
+                  TextSpan(
+                    text: '\n⭐️ $rating',
+                    style: const TextStyle(
+                        fontSize: 14.0,
+                        color: AppColor.fontColor,
+                        fontFamily: 'CenturyGothic'),
+                  )
+                ]),
+          ),
+          trailing: const Icon(
+            Icons.arrow_forward_ios_outlined,
+            color: AppColor.blackColor,
+          )),
+    );
   }
 }
