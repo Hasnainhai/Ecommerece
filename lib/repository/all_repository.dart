30a41b7ec9c9997665d1c34db5153e2373b460e3
Basit ali <basitalyshah51214@gmail.com>
2024-02-21 23:36:06 -0800

    getting all the products

---
+++ lib/repository/all_repository.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:ecommerece/model/home_prod_model.dart';
+import 'package:ecommerece/model/product_detail_model.dart';
+import 'package:ecommerece/utils/routes/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class DataProvider extends ChangeNotifier {
+  List<Category> categoryList = [];
+  List<Product> productList = [];
+
+  Future<void> fetchData(BuildContext context) async {
+    const String url = 'http://zarozar.exarth.com/web/api/product/';
+    const String csrfToken =
+        'TmAfyfkTFQr3aZU6ZkVXS1rGw6rMoRPGqDjwAQIXfRK5q8mK1DXUAWazi7sxZJzK';
+
+    try {
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'accept': 'application/json',
+          'X-CSRFToken': csrfToken,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = json.decode(response.body);
+
+        categoryList = List<Category>.from(
+            data['categories'].map((category) => Category.fromJson(category)));
+        productList = List<Product>.from(
+            data['products'].map((product) => Product.fromJson(product)));
+
+        notifyListeners();
+      } else {
+        if (response.statusCode == 404) {
+          Utils.flushBarErrorMessage("Products not found", context);
+        } else {
+          Utils.flushBarErrorMessage("Unexpected error", context);
+        }
+      }
+    } catch (e) {
+      if (e is SocketException) {
+        Utils.flushBarErrorMessage(
+            "Network error. Check your internet connection.", context);
+      } else if (e is FormatException) {
+        Utils.flushBarErrorMessage("Invalid response format", context);
+      } else {
+        Utils.flushBarErrorMessage("Unexpected error occurred", context);
+      }
+    }
+  }
+}
